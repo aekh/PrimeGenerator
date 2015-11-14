7490f22c1cd69546ba0e21d9456d3b0db8894e29
Alex <djalekzse@gmail.com>
2015-11-14 22:25:39 +0100

    Reduced memory usage somewhat
    Introduced an offset variable to not have to have an array with mostly zeroes

--- primelister.c
+++ primelister.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 // Waring atm this might eat memory.
 
@@ -10,31 +9,37 @@ typedef struct vector {
   unsigned int *arr;
   unsigned long long pos;
   unsigned long long size;
-  //unsigned long long offset;
+  unsigned long long offset;
 } vec;
 
 vec *vec_init(unsigned long long prime) {
-  unsigned long long pos = prime - 1;
-  unsigned long long size = prime*prime;
-  //unsigned long long offset = 0;
+  unsigned long long pos = 0, offset = prime;
+  unsigned long long size = prime;
   unsigned int *arr = calloc(prime*prime, sizeof(unsigned long));
-  for (unsigned long long i = 1; i < prime; ++i) {
-    arr[i * prime - 1] = 1;
+  if (NULL == arr) {printf("Memory error!"); exit(0);}
+
+  for (unsigned long long i = 0; i < size; ++i) {
+    arr[i] = 1;
   } arr[size - 1] = 2;
 
   vec *vect = calloc(1, sizeof(vec));
+  if (NULL == vect) {printf("Memory error!"); exit(0);}
   vect->prime = prime;
   vect->pos = pos;
   vect->size = size;
   vect->arr = arr;
+  vect->offset = offset;
 
   return vect;
-}
+} // [0,1,0,2,0,1,0,3]
+  // [0,1,2,3,4,5,6,7]
+  // [1,2,1,3]
 
 int vec_repeat(vec *vect) {
   // Init new values
   unsigned long long newSize = vect->prime * vect->size;
   unsigned int *newArr = calloc(newSize, sizeof(unsigned long));
+  if (NULL == newArr) {printf("Memory error!"); exit(0);}
   for (unsigned long i = 0; i < vect->prime; ++i) {
     memcpy(newArr + i * vect->size, vect->arr, vect->size * sizeof(unsigned long));
   } newArr[newSize - 1] += 1;
@@ -53,8 +58,23 @@ int vec_repeat(vec *vect) {
   return 0;
 }
 
+unsigned int vec_getNextExp(vec *vect) {
+  unsigned int exponent = 0llu;
+  if (vect->prime == vect->offset) {
+    exponent = vect->arr[vect->pos];
+    if (vect->pos == vect->size - 1) {
+	vec_repeat(vect);
+    } else {
+      vect->pos += 1;
+    }
+    vect->offset = 1;
+  } else {
+    vect->offset += 1;
+  } return exponent;
+}
+
 unsigned long long powll(unsigned long long base, unsigned int exp) {
-  unsigned long long result = 1ULL;
+  unsigned long long result = 1llu;
   while (exp) {
     if (exp & 1) {result *= base;}
     exp >>= 1;
@@ -67,34 +87,30 @@ int primelister(unsigned int primes) {
   // Init
   //unsigned long *primeArr = calloc(primes, sizeof(unsigned long)); primeArr[0] = 2;
   vec **arr = calloc(primes, sizeof(vec *));
+  if (NULL == arr) {printf("Memory error!"); exit(0);}
   unsigned int pos = 1;
-  arr[0] = vec_init(2);
-  unsigned long long prevnum = 0, num = 1;
+  arr[0] = vec_init(2llu);
+  unsigned long long prevnum = 0llu, num = 1llu;
 
   //printf("%u, %u \n", pos, primes);
   
   printf("2, ");
   // Algorithm
   while (pos < primes) {
-    //printf("current pos:%u, primes:%u\n", pos, primes);
     prevnum = num;
-    num = 1;
+    num = 1llu;
     for (int p = 0; arr[p] != NULL; ++p) {
-      num *= powll(arr[p]->prime, arr[p]->arr[arr[p]->pos]);
-      //printf(" ..:: %llu^%u, thus num = %llu.\n", arr[p]->prime, arr[p]->arr[arr[p]->pos], num);
-      if (arr[p]->pos == arr[p]->size - 1) {
-	vec_repeat(arr[p]);
-      } else {
-	arr[p]->pos += 1;
-      }
+      unsigned int exp = vec_getNextExp(arr[p]);
+      num *= powll(arr[p]->prime, exp);
+      //printf(" ..:: %llu^%u, thus num = %llu.\n", arr[p]->prime, exp, num);
     }
-    if (prevnum > num) {
+    if (1 == num) {
       // We found a new prime with value prevnum+1
       unsigned long long newPrime = prevnum + 1;
       //printf("this is what we have, prevnum:%llu . num:%llu . newPrime:%llu and %llu\n", prevnum, num, prevnum+1, newPrime);
       num = newPrime;
       arr[pos] = vec_init(newPrime);
-      arr[pos]->pos += 1;
+      (void) vec_getNextExp(arr[pos]);
       ++pos;
       // Print new prime
       printf("%llu, ", newPrime);
@@ -113,6 +129,6 @@ int primelister(unsigned int primes) {
 int main() {//int argc, char* argv[]) {
   //if (2 != argc) {puts("one argument with number of primes to find.");}
   //unsigned int primes = strtoul(argv[0], NULL, 10);
-  (void) primelister(200);
+  (void) primelister(3631);
   return 0;
 }
